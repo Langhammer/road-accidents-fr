b6414a3316e1a02e4f1b8936327bc41704b3e002
Kay Langhammer <kaylanghammer@outlook.de>
2023-02-20 17:53:01 +0100

    Change notebook format to py:percent

---
+++ code/nb_1.py
@@ -0,0 +1,342 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: roafr
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# Notebook 1: Data Cleaning
+# =========================
+# The purpose of this notebook is to
+# 1. Import the data
+# 2. Clean the data
+# 3. Export the data
+
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+
+import utils
+
+# %%
+# %matplotlib inline
+plt.style.use("dark_background");
+
+# %% [markdown]
+# # Import Dataset
+
+# %%
+dfd = utils.read_csv_of_year(range(2019,2022))
+
+# %% [markdown]
+# # Clean Accident Dataset
+
+# %%
+accidents = pd.merge(left=dfd['characteristics'], 
+                     right=dfd['locations'],
+                     how='outer', 
+                     left_on='Num_Acc', 
+                     right_on='Num_Acc',
+                     validate='one_to_one',
+                     indicator=True)
+
+# Check
+accidents['_merge'].value_counts()
+
+# %% [markdown]
+# ## Translation of French Variable Names
+
+# %%
+accidents.rename(columns={'Num_Acc': 'accident_id', 
+                        'an': 'year',                   # Characteristics Dataset
+                        'mois':'month', 
+                        'jour': 'day', 
+                        'hrmn':'hhmm',
+                        'lum': 'daylight', 
+                        'agg': 'built-up_area', 
+                        'int':'intersection_category', 
+                        'atm': 'weather',
+                        'col': 'collision_category', 
+                        'com': 'municipality', 
+                        'adr':'address', 
+                        'gps': 'gps_origin', 
+                        'lat': 'latitude',
+                        'long': 'longitude', 
+                        'dep': 'department',
+                        'catr': 'road_admin_category',  # Locations Dataset 
+                        'voie': 'lane',
+                        'v1': 'road_numerical_id', 
+                        'v2': 'road_alphanumerical_id', 
+                        'circ': 'traffic_regime', 
+                        'nbv': 'n_lanes', 
+                        'pr': 'landmark', 
+                        'pr1': 'dist_to_landmark',
+                        'vosp': 'reserved_lane',
+                        'prof': 'slope',
+                        'plan': 'plane_layout',
+                        'lartpc': 'median_strip_width',
+                        'larrout': 'affected_road_width',
+                        'surf': 'surface_condition',
+                        'infra': 'infrastructure',
+                        'situ': 'location',
+                        'env1': 'near_school', 
+                        'vma': 'max_speed'},
+                        inplace=True)
+
+# %% [markdown]
+# ## Time and Date-Related Variables
+
+# %%
+
+# Fix inconsistent year format
+accidents['year'].replace({5:2005, 6:2006, 7:2007, 8:2008, 9:2009, 10:2010, 11:2011,
+                                        12:2012, 13:2013, 14:2014, 15:2015, 16:2016, 17:2017, 18:2018},
+                                        inplace=True)
+
+# Fix inconsistent time format
+accidents['hhmm'] = accidents['hhmm'].apply(lambda s: str(s).replace(':',''))
+
+accidents['hour'] = accidents['hhmm'].apply(lambda hhmm: hhmm[:-2])
+accidents['hour'] = accidents['hour'].replace('', np.nan) \
+                                                               .fillna(method='bfill')
+accidents['minute'] = accidents['hhmm'].apply(lambda hhmm: hhmm[-2:])
+
+accidents['date'] = pd.to_datetime({'year':accidents['year'],
+                                                 'month':accidents['month'],
+                                                 'day':accidents['day'],
+                                                 'hour':accidents['hour'],
+                                                 'minute':accidents['minute']})
+
+# New variable: weekday, integer from 0 to 6 representing the weekdays from monday to sunday.
+accidents['day_of_week'] = accidents['date'].apply(lambda x: x.day_of_week)
+
+# New binary variable: is_weekend, 0 for monday to friday and 1 for saturday and sunday
+accidents['is_weekend'] = (accidents['day_of_week'] > 4).astype('int')
+
+
+# %% [markdown]
+# ## Department Variable
+
+# %%
+def department_converter(dep):
+    '''
+    Takes in a department code as int and returns a string
+    e.g. 750 will be '75' for Paris and 201 will be '2B'
+    '''
+    if dep == 201:
+        return '2A'
+    elif dep == 202:
+        return '2B'
+    elif dep>970:
+        return str(dep)
+    else:
+        return str(dep).strip('0')
+
+accidents.loc[(np.less(accidents['year'],2019)),'department'] = \
+    accidents[(np.less(accidents['year'],2019))]['department'].apply(department_converter)
+
+# %% [markdown]
+# ## Other 
+
+# %%
+accidents['weather'] = accidents['weather'].fillna(
+    accidents['weather'].mode()[0])
+accidents['weather'].replace({-1, 0}, inplace=True)
+accidents['weather'].astype('int')
+
+# %%
+accidents['collision_category'] = accidents['collision_category'].fillna(
+    accidents['collision_category'].mode()[0])
+
+# %%
+accidents['built-up_area'].replace({1:0, 2:1}, inplace=True)                                
+
+
+# %%
+def na_stats(df, years=None):
+    if (years is None) & ('year' in df.columns):
+        years = range(df['year'].min(), df['year'].max()+1)
+
+    if isinstance(years, int):
+        years = [years]
+
+    if years is not None:
+        df = df[df['year'].isin(years)]
+
+    na_stats_df = pd.DataFrame(df.isna().sum(), columns=['na_counts'])
+    inverse_n_rows = 1/len(df)
+    na_stats_df['na_ratio'] = na_stats_df['na_counts'].apply(lambda x: x*inverse_n_rows)
+    na_stats_df.rename_axis('variable', inplace=True)
+    na_stats_df = na_stats_df[na_stats_df['na_counts'] != 0]
+    na_stats_df.sort_values(by='na_counts', ascending=False, inplace=True)
+    return na_stats_df
+
+def plot_na_ratio(df=None, na_stats_df=None, years=None):
+    if (na_stats_df is not None) & (df is not None):
+        raise Exception('Only one argument of df and na_stats can be used.')
+    if (na_stats_df is None) & (df is not None):
+        na_stats_df = na_stats(df, years=years)
+
+    sns.barplot(data=na_stats_df,
+                x='na_ratio',
+                y=na_stats_df.index,
+                color='navy');
+    plt.title('Ratio of Missing Values');
+    plt.ylabel('Variable Name');
+    plt.xlabel('Ratio of Missing Values');
+    return na_stats_df
+
+
+# %%
+plot_na_ratio(df=accidents, years=range(2019,2022))
+
+# %% [markdown]
+# # Clean Vehicles Dataset
+
+# %%
+dfd['vehicles'].rename(columns={
+                    'Num_Acc':'accident_id',
+                    'id_vehicule':'unique_vehicle_id',
+                    'num_veh':'vehicle_id',
+                    'senc':'direction',
+                    'catv':'vehicle_category',
+                    'obs':'immobile_obstacle',
+                    'obsm':'mobile_obstacle',
+                    'choc':'impact_point',
+                    'manv':'last_operation',
+                    'motor':'motor_type',
+                    'occutc':'n_occupants'}, 
+                    inplace=True)
+
+# %%
+# Missing values are assigned the vale -1 in the original dataset
+dfd['vehicles'].replace({-1: np.nan}, inplace=True)
+na_stats(dfd['vehicles'])
+
+# %%
+# Every single vehicle Id ends with '01', so we can get rid of it
+print(dfd['vehicles']['vehicle_id'].apply(lambda x: x[-2:]).value_counts())
+dfd['vehicles']['vehicle_id'] = dfd['vehicles']['vehicle_id'].apply(lambda x: x[:-2])
+
+# %%
+# Let's see how many values are not alphabetically encoded 8after removing the numeric part)
+vehicle_id_az = dfd['vehicles']['vehicle_id'].apply(lambda x: re.search(pattern='[^A-Z]', string=x))
+for i_match in range(len(vehicle_id_az)):
+    try:
+        vehicle_id_az[i_match] = vehicle_id_az[i_match].group(0)
+    except AttributeError:
+        pass
+vehicle_id_az.value_counts()
+
+# %%
+dfd['vehicles']['vehicle_id'] = dfd['vehicles']['vehicle_id'].replace(dict.fromkeys(['[',']','\\'], np.nan))
+dfd['vehicles'].isna().sum()
+
+# %% [markdown]
+# As a possible improvement the missing values fo vehicle_id could be inferred from the other vehicle ids.
+# This is not done here, because the variable is possibly not necessary because of the existence of unique_vehicle_id.
+
+# %% [markdown]
+# # Clean Persons Dataset
+
+# %%
+dfd['persons'].rename(columns={
+                        'Num_Acc':'accident_id',
+                        'id_vehicule':'unique_vehicle_id',
+                        'num_veh':'vehicle_id',
+                        'place':'seat',
+                        'catu':'role',
+                        'grav':'severity',
+                        'sexe':'sex',
+                        'an_nais':'year_of_birth',
+                        'trajet':'objective',
+                        'secu1':'safety_1',
+                        'secu2':'safety_2',
+                        'secu3':'safety_3',
+                        'locp':'pedestrian_location',
+                        'actp':'pedestrian_action',
+                        'etatp':'pedestrian_company'
+                        },
+                        inplace=True)
+
+# %%
+# Missing values are assigned the vale -1 in the original dataset
+dfd['persons'].replace({-1: np.nan}, inplace=True)
+na_stats(dfd['persons'])
+
+# %%
+dfd['persons'].dtypes
+
+# %% [markdown]
+# ## Severity
+# The severity is the most important variable, as it will be the target variable for machine learning.
+#
+# In the original database, the severity is encoded as  
+# *(OLD ORDER)*  
+# 1 - Unharmed  
+# 2 - Killed  
+# 3 - Hospitalized  
+# 4 - Minor injury  
+#
+# The "hospitalized" indicator is no longer labelled by the public statistics authority as of 2019
+# [(source)](https://www.data.gouv.fr/en/datasets/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2021/#description).  
+# Therefore, the 'injured' categories will be merged.  
+#
+# The severity categories will also be reordered to make the order more logical:  
+# *(NEW ORDER)*  
+# 1 - Unharmed  
+# 2 - Injured  
+# 3 - Killed  
+
+# %%
+dfd['persons']['severity'].replace({2:3, 3:2, 4:2}, inplace=True)
+dfd['persons']['severity'].fillna(0, inplace=True)
+dfd['persons']['severity'] = dfd['persons']['severity'].astype('int')
+
+# %%
+persons_sample = dfd['persons'].sample(frac=0.001, random_state=0)
+persons_features = persons_sample.select_dtypes(include=np.number).drop(columns=['severity', 'accident_id']).columns
+sns.pairplot(data=persons_sample, 
+             vars=persons_features, 
+             hue='severity', 
+             diag_kind='hist',
+             palette='RdYlGn_r');
+
+# %%
+from pywaffle import Waffle
+fig = plt.figure(
+    FigureClass=Waffle,
+    rows=5,
+    columns=10,
+    values=dfd['persons']['role'].value_counts().sort_index(),
+    labels=['driver', 'passenger', 'pedestrian'],
+    legend={'loc': 'lower left', 'bbox_to_anchor': (0.16, -0.15), 'ncol':3},
+    title={'label':'Role of Involved Persons'}
+)
+
+# %%
+# The variable pedestrian_company has a high number of missing values.
+# This is mostly due to the fact that the number of pedestrians is relatively low
+dfd['persons']['pedestrian_company'][dfd['persons']['role']==3].value_counts(dropna=False)
+
+# %% [markdown]
+# Most of the pedestrians were alone and the missing values are much lower if we only take the pedestrians into account. We can therefore assume that the missing values are mostly '1', if the person is a passenger. The rest will be set to zero.
+
+# %%
+dfd['persons']['pedestrian_company'][dfd['persons']['role']==3] = dfd['persons']['pedestrian_company'][dfd['persons']['role']==3].fillna(1)
+dfd['persons']['pedestrian_company'][dfd['persons']['role']!=3] = dfd['persons']['pedestrian_company'][dfd['persons']['role']!=3].fillna(0)
+
+# %%
+# TODO
+# pedestrian_action: object -> numeric
---
+++ code/nb_2.py
@@ -0,0 +1,44 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: pyfra_env_10
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# Notebook 2: Visualization
+# =========================
+# In this notebook, the data will be visualized and analyzed
+
+# %% [markdown]
+# # Time Series
+
+# %%
+day_time_ticks = (0,300,600,900,1200,1500,1800,2100,2400)
+day_time_tick_labels = ('0:00', '03:00','06:00','09:00','12:00','15:00',
+                   '18:00','21:00','24:00')
+plot_data = pd.DataFrame({'weekdays': (dict_of_category_dfs['characteristics'][dict_of_category_dfs['characteristics']['is_weekend'] != 1 ])['hhmm'],
+                 'weekends': (dict_of_category_dfs['characteristics'][dict_of_category_dfs['characteristics']['is_weekend'] == 1 ])['hhmm']})
+fig= plt.figure();
+sns.histplot(data=plot_data, stat='proportion', bins=24, binrange=(0,2400), common_norm=False);
+plt.xticks(ticks=day_time_ticks, 
+           labels=day_time_tick_labels);
+plt.xlabel('Time of Day')
+plt.xlim((0,2400))
+plt.title('Distribution of Accidents by Daytime', pad = 10)
+
+# %%
+# TODO
+
+# %% [markdown]
+# # Age and Sex
+
+# %%
+# TODO
