97841aeaa6853e2ac5428993fd78d8293066268e
Kay Langhammer <kaylanghammer@outlook.de>
2023-02-23 14:32:07 +0100

    Add geodata plot

--- code/nb_2.py
+++ code/nb_2.py
@@ -22,10 +22,15 @@
 # # Import Modules and Data
 
 # %%
+from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from bokeh.models import ColumnDataSource, HoverTool, WheelZoomTool, DatePicker
+from bokeh.plotting import figure, show
+from bokeh.tile_providers import STAMEN_TONER
+from bokeh.io import output_notebook
 
 import utils
 
@@ -87,3 +92,59 @@ plt.ylim((0,df['age'].max()))
 ax.legend(handles=ax.legend_.legendHandles, labels=['Male', 'Female']);
 ax.set_xticklabels(['driver', 'passenger', 'pedestrian']);
 
+
+# %% [markdown]
+# # Geodata
+
+# %%
+output_notebook()
+earliest_date = df['date'].min().isoformat()
+latest_date = df['date'].max().isoformat()
+date_picker = DatePicker(title='Select Date', value=earliest_date, min_date=earliest_date, max_date=latest_date)
+picked_date = datetime.fromisoformat(date_picker.value)
+show(date_picker)
+
+# %%
+output_notebook()
+
+TOOLTIPS = [
+    ("index", "@accident_id"),
+    ("(lat, lon)", "(@lat, @lon)"),
+    ("severity", "@severity_label"),
+]
+
+p = figure(x_range=(-750_000, 1_125_000), y_range=(5_755_000, 5_955_000),
+           x_axis_type="mercator", y_axis_type="mercator", tooltips=TOOLTIPS)
+p.add_tile(STAMEN_TONER)
+
+# Size of sample of data points to plot. 
+# More than 10_000 data points can become very slow
+plot_df = df[df['date'].apply(datetime.date)==picked_date.date()][['accident_id', 'longitude', 'latitude', 'severity']]
+n_plot = 10_000
+if len(plot_df)>n_plot:
+    plot_df = plot_df.sample(n=n_plot)
+
+colors = plot_df['severity'].replace({1:'blue', 2:'orangered', 3:'red'})
+severity_labels = plot_df['severity'].replace({1:'Unharmed', 2:'Injured', 3:'Killed'})
+markers = plot_df['severity'].replace({1:'circle', 2:'square', 3:'triangle'})
+
+source = ColumnDataSource(data={'accident_id': plot_df['accident_id'],
+                                'lat':plot_df['latitude'],
+                                'lon':plot_df['longitude'],
+                                'severity': plot_df['severity'],
+                                'color':colors,
+                                'severity_label':severity_labels,
+                                'marker':markers})
+
+c = p.circle(x='lat',
+             y='lon', 
+             size=15,
+             fill_alpha=0.8,
+             fill_color='color',
+             line_color='grey',
+             line_width=1,
+             source=source)
+
+hover = HoverTool(tooltips=TOOLTIPS, renderers=[c])
+p.toolbar.active_scroll = p.select_one(WheelZoomTool)
+show(p)
