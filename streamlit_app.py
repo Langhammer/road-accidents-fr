30355e2aae6d85dabe8fd424044d62678cdac14b
Kay Langhammer <kaylanghammer@outlook.de>
2023-02-28 02:48:47 +0100

    Try to fix ModuleNotFoundError on share.streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import streamlit as st
 import streamlit.components.v1 as components
 
-from roaf import data, visualization
+from src.roaf import data, visualization
 
 st.title("Road accidents")
 
